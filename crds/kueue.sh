cf4e831d8dbf8b00467a6f32552155bc60794b8c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2026-09-17 10:42:38 +0200

    feat(crd): add Kueue support
    Adds JSON schema support for Kueue CRDs so the schemastore can validate
    Kueue batch and quota-management resources. Registers the kueue.x-k8s.io
    group in sources.yaml and generates schemas for every kind across both
    served API versions from the upstream release.
    
    Closes: #351
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ crds/kueue.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+export choice=individual
+export FILES=(
+  "kueue.x-k8s.io_admissionchecks.yaml"
+  "kueue.x-k8s.io_clusterqueues.yaml"
+  "kueue.x-k8s.io_cohorts.yaml"
+  "kueue.x-k8s.io_localqueues.yaml"
+  "kueue.x-k8s.io_multikueueclusters.yaml"
+  "kueue.x-k8s.io_multikueueconfigs.yaml"
+  "kueue.x-k8s.io_provisioningrequestconfigs.yaml"
+  "kueue.x-k8s.io_resourceflavors.yaml"
+  "kueue.x-k8s.io_topologies.yaml"
+  "kueue.x-k8s.io_workloadpriorityclasses.yaml"
+  "kueue.x-k8s.io_workloads.yaml"
+)
+
+# renovate: datasource=github-tags depName=kubernetes-sigs/kueue
+export VERSION=0.19.4
+
+function generate_url {
+  local crd_file=$1
+  echo "https://raw.githubusercontent.com/kubernetes-sigs/kueue/v${VERSION}/config/components/crd/bases/${crd_file}"
+}
